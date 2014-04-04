53f113dd9245df116b43dcbbb98b8c83d1cc4bea
Marcin Hoppe <marcin.hoppe@gmail.com>
2014-04-04 14:37:24 +0200

    Add projects and first unit test

---
+++ Src/MarcinHoppe.LangtonsAnts/Lattice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MarcinHoppe.LangtonsAnts
+{
+    public enum Colors
+    {
+        White
+    }
+
+    public class Lattice
+    {
+        public Colors ColorAt(int x, int y)
+        {
+            return Colors.White;
+        }
+    }
+}
---
+++ Test/MarcinHoppe.LangtonsAnts.Tests/LatticeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace MarcinHoppe.LangtonsAnts.Tests
+{
+    public class LatticeTests
+    {
+        Lattice lattice = new Lattice();
+
+        [Fact]
+        public void LatticeOriginIsInitiallyWhite()
+        {
+            Assert.Equal(Colors.White, lattice.ColorAt(0, 0));
+        }
+    }
+}
