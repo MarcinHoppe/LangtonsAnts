97c8894a769cc89bd91aa3449503598342760cf5
Marcin Hoppe <marcin.hoppe@gmail.com>
2014-04-08 10:11:08 +0200

    Simplify Position struct

--- Src/MarcinHoppe.LangtonsAnts/Position.cs
+++ Src/MarcinHoppe.LangtonsAnts/Position.cs
@@ -2,15 +2,10 @@
 
 namespace MarcinHoppe.LangtonsAnts
 {
-    public struct Position : IEquatable<Position>
+    public struct Position
     {
         public int Row { get; private set; }
         public int Column { get; private set; }
-
-        public bool Equals(Position other)
-        {
-            return Row == other.Row && Column == other.Column;
-        }
 
         public override string ToString()
         {
